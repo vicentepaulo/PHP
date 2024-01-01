50b3f079b31731f9bf495c597812247e96282f92
unknown <vicentepaulosjr@outlook.com>
2024-01-01 20:29:57 -0300

    commit

--- conceitos/array/loop-lista.php
+++ conceitos/array/loop-lista.php
@@ -1,9 +1,10 @@
 <?php
 
-$lista = [1, 2, 3, 4, 5];
+$lista = [1, 2, 3, 4, 5, 6, 4];
 
-for ($i = 0; $i < 5; $i++) {
+for ($i = 0; $i < count($lista); $i++) {
     echo $lista[$i] . PHP_EOL;
+
 }
 
 ?>
